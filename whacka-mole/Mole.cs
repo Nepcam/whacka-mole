62c7dae8d347d4cbe3fff7637f87401a98993fc7
Cameron NEPE <cn93@students.waikato.ac.nz>
2020-09-12 22:03:01 +1200

    Mole class set up

--- whacka-mole/Mole.cs
+++ whacka-mole/Mole.cs
@@ -13,5 +13,27 @@ namespace whacka_mole
         private int _y;
         private Color _moleColor;
         private const int SIZE = 50;
+
+        /// <summary>
+        /// Initialises the object to the given values
+        /// </summary>
+        /// <param name="x">The x position of the centre of the mole</param>
+        /// <param name="y">The y position of the centre of the mole</param>
+        /// <param name="moleColor">The colour of the mole</param>
+        public Mole(int x, int y, Color moleColor)
+        {
+            _x = x;
+            _y = y;
+            _moleColor = moleColor;
+        }
+
+        /// <summary>
+        /// Gets all the information about a mole
+        /// </summary>
+        /// <returns>All the information as a neatly padded out string</returns>
+        public override string ToString()
+        {
+            return _x.ToString().PadRight(5) + _y.ToString().PadRight(5) + _moleColor.ToString();
+        }
     }
 }
