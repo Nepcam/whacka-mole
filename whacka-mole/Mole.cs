7dea6519e86a1bca07b9b252d5704792e8a8ff5f
Cameron NEPE <cn93@students.waikato.ac.nz>
2020-09-12 21:56:14 +1200

    Add project files.

---
+++ whacka-mole/Mole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace whacka_mole
+{
+    class Mole
+    {
+        private int _x;
+        private int _y;
+        private Color _moleColor;
+        private const int SIZE = 50;
+    }
+}
